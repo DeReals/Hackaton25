402a670b5f741bb8296808b21e39763071bcf39b
David Ogunbanjo <85774740+DeReals@users.noreply.github.com>
2025-04-05 19:24:48 -0400

    Delete index2.js

--- index2.js
+++
@@ -1,8 +0,0 @@
-function doneHistory(){
-    window.location.href='History.html';
-    document.getElementsByClassName("dbell-1rep").innerHTML = "Hello JavaScript!";
-    document.getElementsByClassName("dbell-2rep").innerHTML = "Hello JavaScript!";
-    document.getElementsByClassName("dbell-3rep").innerHTML = "Hello JavaScript!";
-
-
-}
